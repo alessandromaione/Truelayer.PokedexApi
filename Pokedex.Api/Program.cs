407b6e3259bbe340818439655844e9b005773e90
alessandromaione <33608176+alessandromaione@users.noreply.github.com>
2024-12-13 13:50:19 +0100

    add docker

--- Pokedex.Api/Program.cs
+++ Pokedex.Api/Program.cs
@@ -22,10 +22,6 @@ namespace Pokedex.Api
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-
-            app.UseHttpsRedirection();
-
             app.UseAuthorization();
 
             app.UseSwagger();
